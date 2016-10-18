aa9e841758e114d1423dd576fbeeada82bed5778
qingwengang <qingwengang@qq.com>
2016-10-18 20:57:11 +0800

    123

--- src/SchoolOffline/Controllers/BlogController.cs
+++ src/SchoolOffline/Controllers/BlogController.cs
@@ -13,6 +13,7 @@ namespace SchoolOffline.Controllers
         private CourseService courseService = new CourseService();
         public IActionResult Index()
         {
+            ViewData["typeList"] = courseService.GetDistinctTypeName();
             return View();
         }
         public string GetPageUrl(string type)
@@ -21,8 +22,9 @@ namespace SchoolOffline.Controllers
             StringBuilder sbhtml = new StringBuilder();
             foreach(var item in list)
             {
-                sbhtml.Append("<p><a title=\"霹雳猿教程\" href=\"http://www.piliyuan.com\" target=\"_blank\">霹雳猿教程_<a>");
-                sbhtml.AppendFormat("<a title=\"{1}教程-{0}\" href=\"http://www.piliyuan.com/{1}/{2}.html\" target=\"_blank\">霹雳猿教程-{1}教程-{0}</a>", item.Title, type, item.Id);
+                sbhtml.Append("<p><a title=\"霹雳猿教程\" href=\"http://www.piliyuan.com\" target=\"_blank\">霹雳猿教程<a>");
+                sbhtml.Append("_");
+                sbhtml.AppendFormat("<a title=\"霹雳猿教程_{1}教程-{0}\" href=\"http://www.piliyuan.com/{1}/{2}.html\" target=\"_blank\">-{1}教程-{0}</a>", item.Title, type, item.Id);
                 sbhtml.AppendFormat("<p>");
             }
             return sbhtml.ToString();
